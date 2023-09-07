0b426015885e1add34ca1b928228990b592483cc
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2023-09-07 11:32:18 +0530

    ci(CORE-10): Setup ESLint and fixed bugs

--- lib/loadConfig.ts
+++ lib/loadConfig.ts
@@ -3,13 +3,14 @@ import path from "path";
 
 export const loadConfig = async () => {
   const configPath = path.join(process.cwd(), "ftdconfig.json");
-  let config = {};
+  const config = {};
   try {
     const configFileContent = fs.readFileSync(configPath, "utf-8");
     if (configFileContent.length === 0) throw new Error();
-    config = JSON.parse(configFileContent);
+    await Promise.resolve();
+    //config = JSON.parse(configFileContent);
     console.log(config);
-  } catch (error: any) {
+  } catch (error: unknown) {
     console.error(
       "Config file does not exist run npx @five12days/core init command to create it automatically",
     );
