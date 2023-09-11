52e6be1377240d0cc7a2871970df8c9e29c9fd85
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2023-09-11 17:33:57 +0530

    Setup Commander

--- index.ts
+++ index.ts
@@ -2,3 +2,28 @@
 // import { loadConfig } from "./lib/loadConfig";
 
 // loadConfig();
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .version("1.0.0")
+  .description("An example CLI for managing a directory")
+  .option("-l, --ls  [value]", "List directory contents")
+  .option("-m, --mkdir <value>", "Create a directory")
+  .option("-t, --touch <value>", "Create a file")
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (options.ls) {
+  console.log("LS");
+}
+
+if (options.mkdir) {
+  console.log("MKDIR");
+}
+
+if (options.t) {
+  console.log("T");
+}
