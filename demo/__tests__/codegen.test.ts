e112bf2ccf12bc64e940e96435c9cc7427559fe2
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2023-09-13 15:05:48 +0530

    Created first dummy test

--- demo/__tests__/codegen.test.ts
+++ demo/__tests__/codegen.test.ts
@@ -1,5 +1,17 @@
+import { execCommand } from "../lib/mockCommandInvoke";
+
 describe("CodeGenerationTests", () => {
-  it("Should run codegen command", () => {
-    expect(5).toBe(6);
+  // First clear the generated files if there are any
+  // Then run the generate command for the whole section
+  // After it ran successfully check the generated file contents and paths are correct
+
+  it("Should run codegen command", async () => {
+    const cmdExec = execCommand("echo", ["test"]);
+    const { stdout, stderr, code } = await cmdExec;
+
+    console.log(stdout);
+    expect(stdout).toBe("test\n");
+    expect(stderr).toBe("");
+    expect(code).toBe(0);
   });
 });
