5ada0a1fb5fc4451547693d56dc66b4732652d6e
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2023-10-28 20:18:02 +0530

    Changed validation method

--- types/makeModelParams.ts
+++ types/makeModelParams.ts
@@ -1,9 +1,7 @@
-import { AnyZodObject } from "zod";
-
 import { TValue } from "./repositoryTypes";
 
 export type makeModelParams<T> = {
-  validateModel: (schema: AnyZodObject, model: TRawData) => T;
+  validateModel: (model: TRawData) => T;
 };
 
 export type TRawData = Record<string, TValue>;
