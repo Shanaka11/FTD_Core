6ecd28d62c9e95b1b538440f67a40d8b733a760e
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2023-10-29 09:48:52 +0530

    Handled ZOD error messages

--- lib/validation/zodValidation.ts
+++ lib/validation/zodValidation.ts
@@ -6,11 +6,12 @@ export const validateModelZod = <T>(schema: AnyZodObject, model: TRawData) => {
   try {
     return schema.parse(model) as T;
   } catch (e: unknown) {
-    if (e instanceof ZodError) {
-      e.errors.forEach((error) => {
-        throw new Error(error.message);
-      });
-    }
+    const zodError = e as ZodError;
+
+    zodError?.errors?.forEach((error) => {
+      throw new Error(error.message);
+    });
+
     throw new Error("Validation Error");
   }
 };
