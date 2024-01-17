095dc5341b8c3764c5e7a29e04cfa32614969212
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2024-01-17 12:26:15 +0530

    Fixed issue where multiple statement execution was giving errors

--- lib/db/connecter/mysql/executeQuery.ts
+++ lib/db/connecter/mysql/executeQuery.ts
@@ -19,3 +19,18 @@ export const makeExecuteQuery =
       }
     }
   };
+
+export const makeExecuteMultipleQueries =
+  (connection: mysql.PoolConnection) => async (query: string) => {
+    try {
+      const result = await connection.query(query);
+      return result[0];
+    } catch (e) {
+      if (e instanceof Error) {
+        connection.release();
+        throw new Error(e.message);
+      } else {
+        throw e;
+      }
+    }
+  };
