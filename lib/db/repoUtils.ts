5af1f034da83b6c32d8e1af7c723ca7122d9f846
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2024-01-28 14:59:11 +0530

    Update issues fixed

--- lib/db/repoUtils.ts
+++ lib/db/repoUtils.ts
@@ -2,11 +2,15 @@ import { TRawData } from "../../types/makeModelParams.js";
 import { TValue } from "../../types/repositoryTypes.js";
 import { formatDateToSqlString } from "../codeGen/textUtils.js";
 
-export const getColumnsAndValuesFromModelData = (modelData: TRawData) => {
+export const getColumnsAndValuesFromModelData = (
+  modelData: TRawData,
+  forUpdate: boolean = false,
+) => {
   const columns: string[] = [];
   const values: TValue[] = [];
 
   Object.entries(modelData).forEach(([key, value]) => {
+    if (forUpdate && (key === "ID" || key === "CREATED_AT")) return;
     columns.push(key);
     if (value instanceof Date) {
       values.push(formatDateToSqlString(value));
